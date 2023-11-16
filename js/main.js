55f4eaf0f24e04eefe93aeab99862979e4301e43
weru <147871343+marshaweru@users.noreply.github.com>
2023-11-15 23:13:14 -0800

    Put the correct ID name and format

---
+++ js/main.js
@@ -0,0 +1,4 @@
+//Add some variables
+const dropArea = document.getElementById("drop-area");
+const inputFile = document.getElementById("input-file");
+const imageView = document.getElementById("img-view");
