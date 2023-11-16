29ea9d90d382cc0c213e9b19b27fc959adbaaa57
weru <147871343+marshaweru@users.noreply.github.com>
2023-11-15 23:41:27 -0800

    Comment on removing border

--- js/main.js
+++ js/main.js
@@ -11,6 +11,8 @@ function uploadImage(){
     let imgLink = URL.createObjectURL(inputFile.files[0]);
     imageView.style.backgroundImage = `url(${imgLink})`
     imageView.textContent = "";
+    imageView.style.border = 0;
 }
-//In above code, second-last line img link will be added as img view as a background
-//In the last line we remove the text and icon
+//In above code, third-last line img link will be added as img view as a background
+//In the second-last line we remove the text and icon
+//In the last line we remove the border
