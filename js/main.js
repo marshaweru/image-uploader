2d34b2c287af36a04516681e8a6e662cf0f0eeb3
weru <147871343+marshaweru@users.noreply.github.com>
2023-11-15 23:24:46 -0800

    add event in file input filed

--- js/main.js
+++ js/main.js
@@ -2,3 +2,5 @@
 const dropArea = document.getElementById("drop-area");
 const inputFile = document.getElementById("input-file");
 const imageView = document.getElementById("img-view");
+
+//Add event in input-field
